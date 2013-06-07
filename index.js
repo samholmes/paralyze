39fbf65e984060b1ca382f2893af1481ed7dea4a
Sam Holmes <sam.lb.holmes@gmail.com>
2013-06-06 20:30:29 -0700

    Fixed major issues: 1. Functions returned by check() didn't return the same value as the functions passed to check(). 2. Use cross-platform setImmediate to control flow. 3. Check if the counter immediately after the stack unwinds when creating the a finisher check function.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+if (typeof setImmediate === 'undefined') require('setImmediate');
+
 var finisher = function(done){
     var c = 0;
     return function check(f){
-    	if (typeof f !== 'function') return;
+    	if (typeof f !== 'function') return false;
         ++c;
         return function callback(){
-            f.apply(this, arguments);
-            
-            if (!--c)
-                done();
+            --c;
+            setImmediate(checkCounter);
+            return f.apply(this, arguments);
         };
     }
+    
+    function checkCounter(){
+        if (!c) done();
+    }
+    
+    setImmediate(checkCounter);
 }
 
 module.exports = finisher;
